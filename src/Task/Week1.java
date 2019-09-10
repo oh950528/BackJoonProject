9d754755ce23e34ccadd7a8dcd4187bea3c3d90b
OHILWON <OHILWON@DESKTOP-B720KDO>
2019-09-10 13:51:44 +0900

    2

--- src/Task/Week1.java
+++ src/Task/Week1.java
@@ -1,9 +1,15 @@
 //2.구현 문제
 package Task;
 
+import java.util.Scanner;
+
 public class Week1 {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String str = scan.next().toUpperCase();
+		String arr[];
+		int gap = 0;
 		
 	}
 }
