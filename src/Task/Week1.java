0a1dc5b0cc50ac4c12fa467c757a3695c8497e53
OHILWON <OHILWON@DESKTOP-B720KDO>
2019-09-10 14:52:56 +0900

    dsads

--- src/Task/Week1.java
+++ src/Task/Week1.java
@@ -9,14 +9,18 @@ public class Week1 {
 		Scanner scan = new Scanner(System.in);
 		String str = scan.next().toLowerCase();
 		String arr[];
+		int index = 0;
 		
 		StringBuffer sb = new StringBuffer(str);
 		
 		for(int i = 0; i < str.length(); i++) {
 			if(str.charAt(i) != str.charAt(i - 1)) {
-				sb.insert(i, '/');
+				sb.insert(i + index, '/');
+				index++;
 			}
 		}
+		
+		System.out.println(sb.toString());
 
 	}
 }
