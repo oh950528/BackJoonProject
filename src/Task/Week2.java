493f88aa3b24c4a234b37469aec16ef07e9a7a03
OHILKWON <OHILKWON@DESKTOP-T9GRPAR>
2019-09-16 13:35:18 +0900

    2

--- src/Task/Week2.java
+++ src/Task/Week2.java
@@ -2,9 +2,18 @@
 //백준1138-한 줄로 서기
 package Task;
 
+import java.util.Scanner;
+
 public class Week2 {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int index = scan.nextInt();
 
+		int arr[] = new int[index];
+
+		for(int i = 0; i < index; i++) {
+
+		}
 	}
 }
