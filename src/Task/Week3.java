b314221cf09c800e05fe7dbbc21af7361b7f042b
OHILKWON <OHILKWON@DESKTOP-T9GRPAR>
2019-10-07 15:15:19 +0900

    2

--- src/Task/Week3.java
+++ src/Task/Week3.java
@@ -2,9 +2,23 @@
 //น้มุ1371
 package Task;
 
+import java.util.Scanner;
+
 public class Week3 {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 
+		String str;
+		int[] check = new int[]26;
+
+		int max = 0;
+		int index;
+
+		for(int i = 0; i < check.length; i++) {
+			if(check[i] == max) {
+				System.out.println((char) ('a' + i));
+			}
+		}
 	}
 }
